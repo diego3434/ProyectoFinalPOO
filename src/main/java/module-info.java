66d52bf38c6987a3c268756628fd3954e03cfaef
diego3434 <diegotoapantayanchaguano@gmail.com>
2026-07-05 19:19:49 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.proyectofinalpoo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.proyectofinalpoo to javafx.fxml;
+    exports org.example.proyectofinalpoo;
+}
---
+++ src/main/java/org/example/proyectofinalpoo/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.proyectofinalpoo;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
